be5f00c61a207ee4d4793a41ee43c7ba63f56990
ridhoadtyaa <r2603an@gmail.com>
2022-10-12 21:52:21 +0700

    feat: make indonesia news page

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,38 @@
+import PrimaryButton from './button/PrimaryButton';
+
+const ArticleCard = ({ imgUrl, time, title, desc, source, author, url }) => {
+	return (
+		<div className="overflow-hidden rounded-md bg-white shadow">
+			<img src={imgUrl} alt={title} className="h-40 w-full object-cover object-center" />
+			<div className="flex flex-col justify-between">
+				<div className="border-b py-3 px-5">
+					<div>
+						<span className="font-poppins text-xs">{time}</span>
+						<h5 className="font-secondary text-lg leading-normal">{title}</h5>
+						<p className="mt-5 text-[15px]">{desc}</p>
+					</div>
+					<div className="mt-8">
+						<p className="font-poppins text-sm text-slate-500">
+							By {author} | {source}
+						</p>
+					</div>
+				</div>
+
+				<div className="flex h-full items-center space-x-5  py-3 px-5">
+					<PrimaryButton
+						href={url}
+						target="_blank"
+						className="transiton rounded-sm bg-red-700 py-2 px-4 font-poppins text-sm font-medium text-white duration-300 hover:bg-red-800"
+					>
+						Read More
+					</PrimaryButton>
+					<button className="rounded-sm border border-red-300 py-2 px-4 font-poppins text-sm font-medium text-red-700 transition duration-300 hover:bg-slate-100">
+						Save
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ArticleCard;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import Layout from '../components/layout';
+import axios from 'axios';
+import ArticleCard from '../components/ArticleCard';
+
+const HomePage = () => {
+	const [news, setNews] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const res = await axios.get('https://newsapi.org/v2/top-headlines?country=id&apiKey=ee83a3d108ca49729e5c9932f78a7382');
+
+			setNews(res.data.articles);
+		})();
+	}, []);
+
+	return (
+		<Layout title="Home">
+			<section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+				{news?.map(n => (
+					<ArticleCard
+						key={n.title}
+						imgUrl={n.urlToImage}
+						title={n.title}
+						desc={n.description}
+						source={n.source.name}
+						author={n.author}
+						url={n.url}
+					/>
+				))}
+			</section>
+		</Layout>
+	);
+};
+
+export default HomePage;
