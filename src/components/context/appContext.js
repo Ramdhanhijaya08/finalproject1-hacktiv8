9f83e9844025d6cdd2895aef12e7493293d1556c
Ramdhanhijaya08 <ramdhanhijaya08@gmail.com>
2022-10-18 13:32:28 +0700

    pull

---
+++ src/components/context/appContext.js
@@ -0,0 +1,22 @@
+//  import { createContext } from "react";
+// import { useState } from "react";
+
+
+//  const AppContext = createContext(null);
+
+
+//  const AppContextProvider = ({children}) => {
+//     const [Saved,setSaved] = useState([]);
+
+
+//     const
+
+
+
+//     return{
+//         <AppContext.Provider>
+// {children}
+//         </AppContext.Provider>
+        
+//     }
+//  }
